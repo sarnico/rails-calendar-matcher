ea7f6f4184416b8e2d57090a3be910504a534ec3
Yann Lauwers <yann.lauwers@icloud.com>
2020-03-03 11:28:00 +0100

    forgot to create a branch but added a navbar holly poop

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,19 +7,17 @@ class User < ApplicationRecord
 
   def self.find_for_google_oauth2(auth)
     data = auth.info
-    if validate_email(auth)
-      user = User.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.provider = auth.provider
-        user.uid = auth.uid
-        user.email = auth.info.email
-        user.password = Devise.friendly_token[0,20]
-      end
-      user.token = auth.credentials.token
-      user.refresh_token = auth.credentials.refresh_token
-      user.save
-      return user
-    else
-      return nil
+
+    user = User.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
     end
+    user.token = auth.credentials.token
+    user.refresh_token = auth.credentials.refresh_token
+    user.save
+    return user
+
   end
 end
