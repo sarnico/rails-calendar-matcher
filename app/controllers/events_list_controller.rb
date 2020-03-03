2ecc1539c33e7e38ba03776d77d56e683970eee6
sara nicolay <nicolay.sara@gmail.com>
2020-03-03 15:17:15 +0100

    Controller inutil effacé

--- app/controllers/events_list_controller.rb
+++
@@ -1,9 +0,0 @@
-class EventsListController < ApplicationController
-
-  def index
-  @client = Google::APIClient.new
-  @client.execute(api_method:
-    @service.calendar_list.list)
-    calendars = JSON.parse(response.body)
-  end
-end
