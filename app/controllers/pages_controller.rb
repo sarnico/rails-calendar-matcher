695114d70e425232037534188513921a786180b0
sara nicolay <nicolay.sara@gmail.com>
2020-03-05 13:02:01 +0100

    The match is progressing well

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,22 +12,7 @@ class PagesController < ApplicationController
   end
 
   def test
-    user1 = User.first
-    user2 = User.last
-    set_match = Match.last
-
-
-    set_match.min_date ? t_initial = set_match.min_date : t_initial = Date.today
-    t_final = set_match.max_date
-
-    a = (t_initial..t_final).map do |t|
-      # set_match.users.count do |u|
-      [user1, user2].count do |u|
-        u.user_events.any? do |event|
-          event.start_time.to_date == t
-        end
-      end
-    end
-    raise
+  FindMatch.date
+  raise
   end
 end
