b08929f4c752c1120c8241ee1f6a62a395e820c1
Yann Lauwers <yann.lauwers@icloud.com>
2020-03-09 12:25:46 +0100

    put an alert box with information on click on events. need to make ajdax and update math_date from matches

--- app/javascript/calendar_matches_choice.js
+++ app/javascript/calendar_matches_choice.js
@@ -36,12 +36,60 @@ var calendar_matches = () => {
       },
 
 
-       select: function(info) {
-        console.log(info)
+      select: function(info) {
+        var theDate = new Date(info.start);
+        var theMyDate = theDate.toLocaleDateString();
+
+        const matchingDate = results.find(result => {
+          const date = new Date(result.start)
+          const myDate = date.toLocaleDateString()
+          // console.log(myDate)
+          return myDate === theMyDate
+        })
+
+        // console.log(matchingDate.start)
+
+        if (matchingDate) {
+
+          // Getting Hours, Mins and secs one by one
+          // start time
+          const matchingDateFormatingStart = new Date(matchingDate.start)
+          const matchingDateStartHours = matchingDateFormatingStart.getHours()
+          const matchingDateStartMins = matchingDateFormatingStart.getMinutes()
+          const matchingDateStartSecs = matchingDateFormatingStart.getSeconds()
+
+          // end time
+          const matchingDateFormatingEnd = new Date(matchingDate.end)
+          const matchingDateEndHours = matchingDateFormatingEnd.getHours()
+          const matchingDateEndMins = matchingDateFormatingEnd.getMinutes()
+          const matchingDateEndSecs = matchingDateFormatingEnd.getSeconds()
+
+          // get day date
+          const matchingDateStartDate = matchingDateFormatingStart.toDateString()
+
+          // concatenatin part is gonna be long
+          const startTime = matchingDateStartHours + ':' + matchingDateStartMins + ':' + matchingDateStartSecs
+          const endTime = matchingDateEndHours + ':' + matchingDateEndMins + ':' + matchingDateEndSecs
+
+          // alert box message if selected a possible date
+          const firstMessage = 'Are you sure you want to make your event happen on\n' + matchingDateStartDate + '\n' + startTime + ' until ' + endTime
+
+          const ok = confirm(firstMessage)
+          if (ok == true) {
+
+            console.log('u pressed ok')
+            console.log(matchingDate)
+          }
+
+        } else {
+          alert('No match possible on that day \nPick another date please!')
+        }
+
+
 
                 // var selDate = new Date(start);
                 // add your function
-          },
+      },
 
       // select: function(info) {
       // a = fc-title
