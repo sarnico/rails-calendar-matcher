9aedfbc1ea8e59322297257e332e1e8e63e6362e
sara nicolay <nicolay.sara@gmail.com>
2020-03-09 16:22:43 +0100

    GroupDropDown suit sa route... blur and focus to implement

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -40,22 +40,96 @@ setTimeout( (e)  => {
 }, 1000)
 
 
-const add_attendees = document.getElementById("match_user_ids");
+
 const usersBox = document.getElementById('possible_users')
 const users = JSON.parse(usersBox.dataset.users)
 console.log(users)
 
 
+  const hiddenUsersInput = document.querySelector("#match_user_ids")
+  const userSearchInput = document.querySelector('#shown_user_ids')
+  const selectedUsersDiv = document.querySelector('#selected_user_ids')
+  const userSearchResults = document.querySelector('#user_search_results')
+
+  let filteredUsers = []
+  let selectedUsers = []
+
+  const selectUser = (user) => {
+    if (selectedUsers.indexOf(user) === -1) {
+      selectedUsers.push(user)
+      const paragraph = document.createElement('p')
+      paragraph.appendChild( document.createTextNode(user.email) );
+      paragraph.classList.add('user-chip')
+      selectedUsersDiv.appendChild(paragraph)
+      hiddenUsersInput.value = JSON.stringify(selectedUsers.map(user => user.id))
+    }
+  }
+
+  const clickSelectUser = (event) => {
+    const userId = event.currentTarget.dataset.userId
+    const user = users.find(item => item.id === parseInt(userId))
+    selectUser(user)
+    resetSearch()
+  }
+
+  const search = () => {
+    filteredUsers = []
+    let query = userSearchInput.value;
+    const children = userSearchResults.children
+    for (let i = children.length - 1; i >= 0; i--){
+      const child = children[i]
+      child.remove()
+    }
+
+    users.forEach(user => {
+      if (!user.email.includes(query)) return
+      if (selectedUsers.indexOf(user) != -1) return
+      const paragraph = document.createElement('p')
+      paragraph.appendChild( document.createTextNode(user.email) );
+      paragraph.dataset.userId = user.id;
+      paragraph.addEventListener('click', clickSelectUser)
+      userSearchResults.appendChild(paragraph)
+      filteredUsers.push(user)
+    });
+  }
+
+  const resetSearch = () => {
+    userSearchInput.value = ""
+    search()
+  }
+
+  userSearchInput.addEventListener("input", search);
+
+  userSearchInput.addEventListener("keypress", (event) => {
+    if (event.key === 'Enter') {
+      const user = filteredUsers[0]
+      selectUser(user)
+      resetSearch()
+      event.preventDefault()
+    }
+  })
+
+  const usersAll =  document.querySelectorAll('.user')
+
+  usersAll.forEach((user) => {
+    user.addEventListener("click", clickSelectUser)
+  })
 
 
-  add_attendees.addEventListener("keyup", (event) => {
-    let insertedValue = event.currentTarget.value;
-    console.log(insertedValue)
-    console.log(users)
-    users.forEach(user => console.log(user.email))
 
-    // if user.start_with?('insertedValue')){
-    //   return "Yeah";
-    // };
-  });
+  // userSearchResults.addEventListener("click", (event) => {
+  //   // const user = filteredUsers[0]
+  //   // console.log(event.currentTarget)
+  //   const user = document.querySelector("#user_search_results p").dataset.userId
+  //   console.log(user)
+  //   if (selectedUsers.indexOf(user) === -1) {
+  //     selectedUsers.push(user)
+  //     const paragraph = document.createElement('p')
+  //     paragraph.appendChild( document.createTextNode(user.email) );
+  //     paragraph.classList.add('user-chip')
+  //     selectedUsersDiv.appendChild(paragraph)
+  //     hiddenUsersInput.value = JSON.stringify(selectedUsers.map(user => user.id))
+  //   event.preventDefault()
+  // }
+// })
 
