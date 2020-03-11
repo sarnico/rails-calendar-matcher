77d82d7fb613b0d13216748943dba8dcc155bd4a
Yann Lauwers <yann.lauwers@icloud.com>
2020-03-11 14:39:53 +0100

    refresh token not really working. refreshes all the time long but need to refresh only when the access token is not valid anymore

--- app/services/google_refresh.rb
+++ app/services/google_refresh.rb
@@ -1,12 +1,54 @@
 class GoogleRefresh
 
+ def self.to_params(current_user)
+    { 'refresh_token' => current_user.refresh_token,
+      'client_id'     => ENV['GOOGLE_CLIENT_ID'],
+      'client_secret' => ENV['GOOGLE_CLIENT_SECRET'],
+      'grant_type'    => 'refresh_token'}
+  end
+
+  def self.request_token_from_google(current_user)
+    url = URI("https://accounts.google.com/o/oauth2/token")
+    Net::HTTP.post_form(url, self.to_params(current_user))
+  end
+
+  def self.refresh!(current_user)
+    data = JSON.parse(request_token_from_google(current_user).body)
+    binding.pry
+    current_user.token  = data['access_token'],
+    current_user.save
+    binding.pry
+
+  end
+
+    def self.connect_service
+      @client = auth_client
+      service = ::Google::Apis::CalendarV3::CalendarService.new
+      service.authorization = @client
+      service
+    end
+    def self.auth_client(current_user)
+      Signet::OAuth2::Client.new(
+        client_id: ENV['GOOGLE_CLIENT_ID'],
+        client_secret:  ENV['GOOGLE_CLIENT_SECRET'],
+        token_credential_uri: "https://accounts.google.com/o/oauth2/token",
+        access_token: current_user.token,
+        refresh_token: current_user.refresh_token,
+        grant_type: "authorization_code"
+      )
+    end
+
   def self.refresh_all(current_user, min_date = DateTime.now.rfc3339, max_date = nil)
     current_user.user_events.destroy_all
-
-    authorization = Signet::OAuth2::Client.new(access_token: current_user.token)
+    #refresh!(current_user)
+    authorization = auth_client(current_user)
+    response = authorization.refresh!
+    current_user.token = response["access_token"]
+    current_user.refresh_token = response["refresh_token"]
+    current_user.save
+    authorization = auth_client(current_user)
     service = Google::Apis::CalendarV3::CalendarService.new
     service.authorization = authorization
-
     calendar_id = "primary"
 
     if max_date == nil
