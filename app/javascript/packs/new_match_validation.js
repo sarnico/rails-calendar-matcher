1858ced75ef0cca4d9edcb7ba71f0707d7f28793
Yann Lauwers <yann.lauwers@icloud.com>
2020-05-30 11:06:21 +0200

    hour min must be lower than hour max

--- app/javascript/packs/new_match_validation.js
+++ app/javascript/packs/new_match_validation.js
@@ -45,88 +45,100 @@ const newMatchValidation = () => {
         }
 
         // //hours
-        // const hourMinErrorHTML = document.querySelector(".hour-min-invalid-error-message")
-        // const hourMinBelowErrorHTML = document.querySelector(".hour-min-below-error-message")
-        // const doublePointMin = document.querySelector(".double-point-min");
-        // const hourMinInput = document.querySelector(".hour-min-input")
-        // const minMinInput = document.querySelector(".min-min-input")
-        // const hourMaxErrorHTML = document.querySelector(".hour-max-error-message")
-        // const doublePointMax = document.querySelector(".double-point-max");
-        // const hourMaxInput = document.querySelector(".hour-max-input")
-        // const minMaxInput = document.querySelector(".min-max-input")
-        // if (
-        //     hourMinInput.value.length === 0 ||
-        //     minMinInput.value.length === 0 ||
-        //     minMinInput.value.length > 2 ||
-        //     hourMinInput.value.length > 2
-        // ) {
-        //     event.preventDefault();
-        //     hourMinInput.style.borderLeft = "solid 1px #FD0F15";
-        //     hourMinInput.style.borderTop = "solid 1px #FD0F15";
-        //     hourMinInput.style.borderBottom = "solid 1px #FD0F15";
-        //     hourMinInput.style.borderRadiusLeft = "2px";
-        //     hourMinInput.style.outline = "none";
-        //     minMinInput.style.borderRight = "solid 1px #FD0F15";
-        //     minMinInput.style.borderTop = "solid 1px #FD0F15";
-        //     minMinInput.style.borderBottom = "solid 1px #FD0F15";
-        //     minMinInput.style.borderRadiusRight = "2px";
-        //     doublePointMin.style.borderTop = "solid 1px #FD0F15";
-        //     doublePointMin.style.borderBottom = "solid 1px #FD0F15";
-        //     hourMinErrorHTML.style.display = "contents";
-        // } else {
-        //     hourMinInput.style.borderLeft = "solid 1px #A7D930";
-        //     hourMinInput.style.borderTop = "solid 1px #A7D930";
-        //     hourMinInput.style.borderBottom = "solid 1px #A7D930";
-        //     hourMinInput.style.borderRadiusLeft = "2px";
-        //     minMinInput.style.borderRight = "solid 1px #A7D930";
-        //     minMinInput.style.borderTop = "solid 1px #A7D930";
-        //     minMinInput.style.borderBottom = "solid 1px #A7D930";
-        //     minMinInput.style.borderRadiusRight = "2px";
-        //     doublePointMin.style.borderTop = "solid 1px #A7D930";
-        //     doublePointMin.style.borderBottom = "solid 1px #A7D930";
-        //     hourMinErrorHTML.style.display = "none";
-        // }
-        // if (
-        //     hourMaxInput.value.length === 0 ||
-        //     minMaxInput.value.length === 0 ||
-        //     minMaxInput.value.length > 2 ||
-        //     hourMaxInput.value.length > 2
-        // ) {
-        //     event.preventDefault();
-        //     hourMaxInput.style.borderLeft = "solid 1px #FD0F15";
-        //     hourMaxInput.style.borderTop = "solid 1px #FD0F15";
-        //     hourMaxInput.style.borderBottom = "solid 1px #FD0F15";
-        //     hourMaxInput.style.borderRadiusLeft = "2px";
-        //     minMaxInput.style.borderRight = "solid 1px #FD0F15";
-        //     minMaxInput.style.borderTop = "solid 1px #FD0F15";
-        //     minMaxInput.style.borderBottom = "solid 1px #FD0F15";
-        //     minMaxInput.style.borderRadiusRight = "2px";
-        //     doublePointMax.style.borderTop = "solid 1px #FD0F15";
-        //     doublePointMax.style.borderBottom = "solid 1px #FD0F15";
-        //     hourMaxErrorHTML.style.display = "contents";
-        // } else {
-        //     hourMaxInput.style.borderLeft = "solid 1px #A7D930";
-        //     hourMaxInput.style.borderTop = "solid 1px #A7D930";
-        //     hourMaxInput.style.borderBottom = "solid 1px #A7D930";
-        //     hourMaxInput.style.borderRadiusLeft = "2px";
-        //     minMaxInput.style.borderRight = "solid 1px #A7D930";
-        //     minMaxInput.style.borderTop = "solid 1px #A7D930";
-        //     minMaxInput.style.borderBottom = "solid 1px #A7D930";
-        //     minMaxInput.style.borderRadiusRight = "2px";
-        //     doublePointMax.style.borderTop = "solid 1px #A7D930";
-        //     doublePointMax.style.borderBottom = "solid 1px #A7D930";
-        //     hourMaxErrorHTML.style.display = "none";
-        // }
+        const hourMinErrorHTML = document.querySelector(".hour-min-invalid-error-message")
+        const doublePointMin = document.querySelector(".double-point-min");
+        const hourMinInput = document.querySelector(".hour-min-input")
+        const minMinInput = document.querySelector(".min-min-input")
+
+        const hourMaxErrorHTML = document.querySelector(".hour-max-error-message")
+        const doublePointMax = document.querySelector(".double-point-max");
+        const hourMaxInput = document.querySelector(".hour-max-input")
+        const minMaxInput = document.querySelector(".min-max-input")
+            // if (
+            //     hourMinInput.value.length === 0 ||
+            //     minMinInput.value.length === 0 ||
+            //     minMinInput.value.length > 2 ||
+            //     hourMinInput.value.length > 2
+            // ) {
+            //     event.preventDefault();
+            //     hourMinInput.style.borderLeft = "solid 1px #FD0F15";
+            //     hourMinInput.style.borderTop = "solid 1px #FD0F15";
+            //     hourMinInput.style.borderBottom = "solid 1px #FD0F15";
+            //     hourMinInput.style.borderRadiusLeft = "2px";
+            //     hourMinInput.style.outline = "none";
+            //     minMinInput.style.borderRight = "solid 1px #FD0F15";
+            //     minMinInput.style.borderTop = "solid 1px #FD0F15";
+            //     minMinInput.style.borderBottom = "solid 1px #FD0F15";
+            //     minMinInput.style.borderRadiusRight = "2px";
+            //     doublePointMin.style.borderTop = "solid 1px #FD0F15";
+            //     doublePointMin.style.borderBottom = "solid 1px #FD0F15";
+            //     hourMinErrorHTML.style.display = "contents";
+            // } else {
+            //     hourMinInput.style.borderLeft = "solid 1px #A7D930";
+            //     hourMinInput.style.borderTop = "solid 1px #A7D930";
+            //     hourMinInput.style.borderBottom = "solid 1px #A7D930";
+            //     hourMinInput.style.borderRadiusLeft = "2px";
+            //     minMinInput.style.borderRight = "solid 1px #A7D930";
+            //     minMinInput.style.borderTop = "solid 1px #A7D930";
+            //     minMinInput.style.borderBottom = "solid 1px #A7D930";
+            //     minMinInput.style.borderRadiusRight = "2px";
+            //     doublePointMin.style.borderTop = "solid 1px #A7D930";
+            //     doublePointMin.style.borderBottom = "solid 1px #A7D930";
+            //     hourMinErrorHTML.style.display = "none";
+            // }
+            // if (
+            //     hourMaxInput.value.length === 0 ||
+            //     minMaxInput.value.length === 0 ||
+            //     minMaxInput.value.length > 2 ||
+            //     hourMaxInput.value.length > 2
+            // ) {
+            //     event.preventDefault();
+            //     hourMaxInput.style.borderLeft = "solid 1px #FD0F15";
+            //     hourMaxInput.style.borderTop = "solid 1px #FD0F15";
+            //     hourMaxInput.style.borderBottom = "solid 1px #FD0F15";
+            //     hourMaxInput.style.borderRadiusLeft = "2px";
+            //     minMaxInput.style.borderRight = "solid 1px #FD0F15";
+            //     minMaxInput.style.borderTop = "solid 1px #FD0F15";
+            //     minMaxInput.style.borderBottom = "solid 1px #FD0F15";
+            //     minMaxInput.style.borderRadiusRight = "2px";
+            //     doublePointMax.style.borderTop = "solid 1px #FD0F15";
+            //     doublePointMax.style.borderBottom = "solid 1px #FD0F15";
+            //     hourMaxErrorHTML.style.display = "contents";
+            // } else {
+            //     hourMaxInput.style.borderLeft = "solid 1px #A7D930";
+            //     hourMaxInput.style.borderTop = "solid 1px #A7D930";
+            //     hourMaxInput.style.borderBottom = "solid 1px #A7D930";
+            //     hourMaxInput.style.borderRadiusLeft = "2px";
+            //     minMaxInput.style.borderRight = "solid 1px #A7D930";
+            //     minMaxInput.style.borderTop = "solid 1px #A7D930";
+            //     minMaxInput.style.borderBottom = "solid 1px #A7D930";
+            //     minMaxInput.style.borderRadiusRight = "2px";
+            //     doublePointMax.style.borderTop = "solid 1px #A7D930";
+            //     doublePointMax.style.borderBottom = "solid 1px #A7D930";
+            //     hourMaxErrorHTML.style.display = "none";
+            // }
 
 
-
-        // if (
-        //     hourMinInput.value > hourMaxInput.value ||
-        //     (minMinInput.value > minMaxInput.value && hourMinInput.value === hourMaxInput.value) ||
-        //     (minMinInput.value === minMaxInput.value && hourMinInput.value === hourMaxInput.value)
-        // ) {
-        //     hourMinBelowErrorHTML.classList.add("hour-min-below-error-message-red")
-        // }
+        const rightHour = document.getElementById("right-hour")
+        if (
+            (
+                (hourMinInput.value.length !== 0) && (hourMaxInput.value.length !== 0) &&
+                (minMinInput.value.length !== 0) && (minMaxInput.value.length !== 0)
+            ) &&
+            (
+                hourMinInput.value > hourMaxInput.value ||
+                (minMinInput.value > minMaxInput.value && hourMinInput.value === hourMaxInput.value) ||
+                (minMinInput.value === minMaxInput.value && hourMinInput.value === hourMaxInput.value)
+            )) {
+            event.preventDefault()
+            if (rightHour.classList.contains("error-green")) {
+                rightHour.classList.remove("error-red")
+            }
+            rightHour.classList.add("error-red")
+            rightHour.style.display = "contents";
+        } else {
+            rightHour.style.display = "none";
+        }
 
         // for attendee validation, go have a look on user dropdown on the bottom
     })
