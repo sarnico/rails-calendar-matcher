04e69fac9e789ce06851347119bca9ce8491c8b9
sara nicolay <nicolay.sara@gmail.com>
2020-06-09 20:02:47 +0200

    websocket-extensions (0.1.4)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,6 +96,3 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
-
-config.logger = Logger.new(STDOUT)
-config.logger.level = Logger::DEBUG
