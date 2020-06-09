9efce718e4894ecdfd7e0ab4813addd9fede15f0
sara nicolay <nicolay.sara@gmail.com>
2020-06-09 16:22:26 +0200

    ok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,6 +53,7 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
@@ -95,3 +96,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+config.logger = Logger.new(STDOUT)
+config.logger.level = Logger::DEBUG
